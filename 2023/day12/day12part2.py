65ce42772aa0e5ae2947d91274587f909a86b92f
Robert Goble <goble_robert@bah.com>
2023-12-13 02:38:20 -0500

    Finished day12 part 2

--- 2023/day12/day12part2.py
+++ 2023/day12/day12part2.py
@@ -1,20 +1,18 @@
+from functools import cache
 from itertools import product
 
-allFound = []
-def getCombos(springs, data, currLen, s):
-    global allFound
+@cache
+def getCombos(springs, data, currLen):
     # print("getCombos([%s],%s,%d) => '%s'" % (springs, data, currLen, s))
     # Check end conditions    
     if len(springs) == 0:        
         if len(data) == 0 and currLen == 0:
             # Exhausted both so its valid
             # print("\tFound #1 => '%s'" % s)
-            allFound.append(s)
             return 1
         elif len(data) == 1 and currLen == data[0]:
             # matched the last data
             # print("\tFound #2 => '%s'" % s)
-            allFound.append(s)
             return 1
         else:            
             # Out of springs but still have data left so this
@@ -24,23 +22,21 @@ def getCombos(springs, data, currLen, s):
 
     # its either a blank, or the end of a run of springs    
     if springs[0] == '.':
-        s += '.'
         if currLen == 0:
             # This is a working spring, and not part of block
             # so just continue processing
-            return getCombos(springs[1:], data, 0, s)
+            return getCombos(springs[1:], data, 0)
         elif len(data) > 0 and currLen == data[0]:
             # This is the end of a run bad springs and it matches
             # our required size so it's good so far. So just Keep
             # processing the rest of the strings and data
-            return getCombos(springs[1:], data[1:], 0, s)
+            return getCombos(springs[1:], data[1:], 0)
         else:
             # This is the end of a run and it didn't match
             # the needed size, so its a failed possibility
             # print("Data exhausted #1 => '%s'" % s)
             return 0
     elif springs[0] == '#':
-        s += '#'
         # Increment the currLen
         currLen += 1
         if len(data) == 0:
@@ -50,7 +46,7 @@ def getCombos(springs, data, currLen, s):
 
         if currLen <= data[0]:
             # It could still be a valid solution so keep going
-            return getCombos(springs[1:], data, currLen, s)
+            return getCombos(springs[1:], data, currLen)
         else:
             # This pushes us over the max size for this block
             # so it's a failed combo
@@ -63,19 +59,19 @@ def getCombos(springs, data, currLen, s):
 
         if currLen == 0:
             # Not in a run so just continue as normal
-            sum = getCombos(springs[1:], data, 0, s+'.')
+            sum = getCombos(springs[1:], data, 0)
         elif len(data) > 0 and currLen == data[0]:
             # If we were in the middle of a run and treat it as working
             # we can end the run and continue processing
             # print("\tTreating as good!")
-            sum = getCombos(springs[1:], data[1:], 0, s+'.')
+            sum = getCombos(springs[1:], data[1:], 0)
         elif len(data) > 0 and currLen > data[0]:
             # Its invalid
             # print("Invalid #1 => '%s'" % s)
             return 0
         
         # We also want to treat it as broken and count those possibilites        
-        return sum + getCombos(springs[1:], data, currLen+1,s+'#')
+        return sum + getCombos(springs[1:], data, currLen+1)
     
 ans = 0
 # with open("sample.txt", "rt") as inp:
@@ -84,16 +80,14 @@ with open("input.txt", "rt") as inp:
         line = line.strip()
         springs, data = line.split(" ")
         
-        springs = '?'.join(springs for x in range(5))
-        data = ','.join(data for x in range(5))
-        data = [int(x) for x in data.split(',')]
+        springs = '?'.join([springs]*5)
+        data = tuple(int(x) for x in data.split(',')) * 5
         # print("\tx5 springs = %s  ==> Data: %s" % (springs, data))
 
         # if line != "?###???????? 3,2,1":
         #     continue
 
-        s = ''
-        combos = getCombos(springs, data, 0, s)
+        combos = getCombos(springs, data, 0)
         ans += combos
         print("%s ==> %d combos" % (line, combos))
         # for x in allFound:
